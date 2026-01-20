d851c879bbfe58b88e4057b655a593f99f9ccb24
nsufiyan <shaiknizamuddinsufiyan@gmail.com>
2026-01-20 18:41:25 +0530

    update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
   require("express-session")({
     secret: process.env.SESSION_SECRET,
     cookie: {
-      maxAge: 1000 * 60 * 60,//1 hour
+      maxAge: 1000 * 60 * 60,//1 hour 
       httpOnly: true,
       secure: false,
     },
