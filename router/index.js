e68fac805bac79dd69683c67ff195a931f8c2370
nsufiyan <shaiknizamuddinsufiyan@gmail.com>
2026-02-18 23:38:41 +0530

    router index

---
+++ router/index.js
@@ -0,0 +1,4 @@
+const UserRouter = require("./userRoute");
+const ComplaintRouter = require("./complaintRouter");
+const ContactUsRouter = require("./contactusRouter")
+module.exports = { UserRouter, ComplaintRouter, ContactUsRouter };
