b05572b0273a3e7a7399fc82b509a7340795c91b
nsufiyan <shaiknizamuddinsufiyan@gmail.com>
2026-02-25 22:24:26 +0530

    index controller

---
+++ controller/index.js
@@ -0,0 +1,4 @@
+const UserController = require("./userController");
+const ComplaintController = require("./complaintController");
+const ContactUsController = require("../controller/contactUsController")
+module.exports = { UserController, ComplaintController, ContactUsController };
