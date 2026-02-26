d08980ee29cf0e14c6f6403e2d5bc08e7d1ab628
nsufiyan <shaiknizamuddinsufiyan@gmail.com>
2026-02-26 23:25:13 +0530

    vite config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    //  host: true,
+    host: true,
     port: 5000,
   },
   plugins: [react()],
